f4bbd1ee1ad9a9f7a13d0db2713490827e7d1beb
ASandhuZero <as7687a@student.american.edu>
2017-04-01 22:47:18 -0400

    Always use println to print shit to the console.

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -4,4 +4,6 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "I'm a little teapot!"))
+  (println "I'm a little teapot!")
+  (do (println "No prompt here!")
+  (print+ 1 3)))
