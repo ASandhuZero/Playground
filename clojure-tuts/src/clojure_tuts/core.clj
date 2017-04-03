11d52ff54557cbece714926e8378c4df73311e58
ASandhuZero <as7687a@american.edu>
2017-04-03 14:07:43 -0400

    Learning about clojure namespacing and how to figure out where the fuck I am at all times. Might look into getting a repl for atom. Because goddamn it is annoying to run this shit on a terminal over and over again.

--- clojure-tuts/src/clojure_tuts/core.clj
+++ clojure-tuts/src/clojure_tuts/core.clj
@@ -1,4 +1,5 @@
 (ns clojure-tuts.core
+  (:require [clojure-tuts.core :as tuts])
   (:gen-class))
 
   (defn -example
@@ -6,10 +7,19 @@
     "Just some random function"
     (println (str "Hello" " World"))
     (println (+ 1 2 )))
+    (println *ns*) ; This namespace is clojure-tuts.core.
 ; (-example)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   ; (-example) I wonder if there is a way to get a function in there.
-  (clojure-tuts.core.-example)
+  ; (clojure-tuts.core.-example) This doesn't work because namespaces.
+  ; (tuts.-example)
+  (println *ns*) ; This namespace is user! Not clojure-tuts.core!
+  (println (find-ns 'clojure-tuts.core))
+  (println (all-ns))
+  (defn example1
+    []
+    (println "This runs because namespaces!"))
+  (example1)
   (println "Hello, World!"))
