a9ae017f44b634dcf824f3ce499e641976b148cc
ASandhuZero <as7687a@american.edu>
2017-04-03 13:44:14 -0400

    Trying to still understand how Clojure's namespace works.

--- clojure-tuts/src/clojure_tuts/core.clj
+++ clojure-tuts/src/clojure_tuts/core.clj
@@ -1,7 +1,15 @@
 (ns clojure-tuts.core
   (:gen-class))
 
+  (defn -example
+    []
+    "Just some random function"
+    (println (str "Hello" " World"))
+    (println (+ 1 2 )))
+; (-example)
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
+  ; (-example) I wonder if there is a way to get a function in there.
+  (clojure-tuts.core.-example)
   (println "Hello, World!"))
