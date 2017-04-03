81ab89db9aad68166657d41fc24a67e9965aae3e
ASandhuZero <as7687a@american.edu>
2017-04-03 13:18:59 -0400

    Made a new directory for clojure tutorials.

---
+++ clojure-tuts/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-tuts
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure-tuts/test/clojure_tuts/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-tuts.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-tuts.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
