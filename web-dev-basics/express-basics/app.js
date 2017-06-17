7c6041b38d4476a513969ea6d3a0f06515a4c73f
asandhuzero <as7687a@student.american.edu>
2017-06-17 01:04:31 -0400

    Ran into the MongoDB part of the tutorial and I really don't want to mess with that right now.

--- web-dev-basics/express-basics/app.js
+++ web-dev-basics/express-basics/app.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+var expressValidator = require('express-validator');
 
 var app = express();
 
-var logger = function(req, res, next) {
-    console.log('Logging...');
-    next();
-}
-
-app.use(logger);
+// var logger = function(req, res, next) {
+//     console.log('Logging...');
+//     next();
+// }
+//
+// app.use(logger);
 
 // View Engine
 app.set('view engine', 'ejs');
@@ -21,6 +22,30 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // Set Static path
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Global Vars
+app.use(function(req, res, next) {
+    res.locals.errors = null;
+    next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'),
+      root = namespace.shift(),
+      formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 var users = [
     {
@@ -41,12 +66,38 @@ var users = [
         last_name:'Donuts',
         email:'OP@TM.COM'
     }
-]
+];
 app.get('/', function(req, res) {
     res.render('index', {
         title: 'Customers',
         users: users
     });
+});
+
+app.post('/users/add', function(req, res) {
+
+    req.checkBody('first_name', 'First name is required').notEmpty();
+    req.checkBody('last_name', 'Last name is required').notEmpty();
+    req.checkBody('email', 'Email is required').notEmpty();
+
+    var errors = req.validationErrors();
+
+    if(errors) {
+        res.render('index', {
+            title:'Customers',
+            users:users,
+            errors:errors
+        });
+    } else {
+        var newUser = {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email
+        };
+        console.log('SUCCESS');
+    }
+    res.redirect('/');
+
 });
 
 app.listen(3000, function() {
