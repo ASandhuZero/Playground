1b5879991f40051d356eeb823bc0cc0a21845914
asandhuzero <as7687a@student.american.edu>
2017-08-21 00:51:58 -0400

    Learning is happening. Slowly but surely.

---
+++ Traverse-Vue/src/components/Test.vue
@@ -0,0 +1,16 @@
+<template id="Test">
+  <div class="Test">
+    <h1>{{ msg }}</h1>
+  </div>
+
+</template>
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+      msg: 'Test'
+    }
+  }
+}
+</script>
