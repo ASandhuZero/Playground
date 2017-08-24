8a559525f28bae2fb194ebf2c71f328e53cbb4fd
asandhuzero <as7687a@student.american.edu>
2017-08-24 18:35:05 -0400

    Changed Test.vue to Home.vue for proper descriptive naming.

---
+++ Traverse-Vue/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+    <h1>Traverse</h1>
+    <p>This is the home page for Traverse</p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
