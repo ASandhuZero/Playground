f14b77ad542632f3b5ef70f0c026f2f3251d9555
asandhuzero <as7687a@student.american.edu>
2017-08-25 18:06:03 -0400

    Added a path route to Home page.

--- Traverse-Vue/src/components/About.vue
+++ Traverse-Vue/src/components/About.vue
@@ -3,6 +3,7 @@
     <h1>About</h1>
     <p>Traverse is an augmented reality experience for the Smithsonian American article
     Museum.</p>
+    <li><router-link to="/">Home</router-link></li>
   </div>
 
 </template>
