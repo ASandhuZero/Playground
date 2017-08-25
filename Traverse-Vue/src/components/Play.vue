e3aba859b7e5dc189af37cf18685db58f69e17ef
asandhuzero <as7687a@student.american.edu>
2017-08-25 18:06:54 -0400

    Added path route to Home.

--- Traverse-Vue/src/components/Play.vue
+++ Traverse-Vue/src/components/Play.vue
@@ -1,13 +1,23 @@
 <template lang="html">
   <div>
     <h1>Play</h1>
+    <div class="text_box">
+      <p>Hello</p>
+    </div>
+    <li><router-link to="/">Home</router-link></li>
   </div>
 </template>
 
 <script>
-export default {
-}
 </script>
 
 <style lang="css">
+  .text_box {
+    margin: auto;
+    height: 80%;
+    width: 80%;
+    border-style: double;
+    text-align: center;
+
+  }
 </style>
