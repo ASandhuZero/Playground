554fdbd8c08c94ad8bf9db0467c869a926f9f4e5
asandhuzero <as7687a@student.american.edu>
2017-08-25 18:06:33 -0400

    Added paths routes to Play and About..

--- Traverse-Vue/src/components/Home.vue
+++ Traverse-Vue/src/components/Home.vue
@@ -2,6 +2,9 @@
   <div>
     <h1>Traverse</h1>
     <p>This is the home page for Traverse</p>
+    <li><router-link to="/About">About</router-link></li>
+    <li><router-link to="/Play">Play</router-link></li>
+
   </div>
 </template>
 
