fc2a840c9c3a7681634501070adea716a87f0ed5
asandhuzero <as7687a@student.american.edu>
2017-08-24 20:16:08 -0400

    Router is now bare bones functional with three routes. Home page, About page, and the Hello page that comes with the the webpack install of Vue

--- Traverse-Vue/src/router/index.js
+++ Traverse-Vue/src/router/index.js
@@ -1,27 +1,38 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import Hello from '@/components/Hello'
 import Home from '@/components/Home'
 import About from '@/components/About'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
   routes: [
-    {
-      path: '/Hello',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/About',
-      name: 'About',
-      component: About
-    }
+    {path: '/', component: Home},
+    {path: '/About', component: About},
+    {path: '/Hello', component: Hello}
   ]
 })
+
+export default router
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/Hello',
+//       name: 'Hello',
+//       component: Hello
+//     },
+//     {
+//       path: '/',
+//       name: 'Home',
+//       component: Home
+//     },
+//     {
+//       path: '/About',
+//       name: 'About',
+//       component: About
+//     }
+//   ]
+// })
